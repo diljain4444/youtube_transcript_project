dff27d0881df670004e3b3c83a6d878f9a42fd86
diljain4444 <diljain121@gmail.com>
2026-04-11 13:09:51 +0530

    link updated

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-API_BASE ="https://jain4444-yt-insight-api.hf.space"
+API_BASE = "http://localhost:8000"
 
 # ── Custom CSS ───────────────────────────────────────────────────────
 st.markdown("""
